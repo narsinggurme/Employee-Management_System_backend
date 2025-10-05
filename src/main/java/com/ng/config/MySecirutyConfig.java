cc0dfa3d5f8d3561bd668d3200735db47d1be345
narsinggurme <narsinggurme@gmail.com>
2025-10-05 09:16:39 +0530

    final backedn code for ems

--- src/main/java/com/ng/config/MySecirutyConfig.java
+++ src/main/java/com/ng/config/MySecirutyConfig.java
@@ -50,7 +50,9 @@ public class MySecirutyConfig
 	public CorsConfigurationSource corsConfigurationSource()
 	{
 		CorsConfiguration configuration = new CorsConfiguration();
-		 configuration.setAllowedOrigins(List.of(
+//		configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+
+		 configuration.setAllowedOrigins(List.of( 
 			        "http://localhost:4200", 
 			        "https://emsappbynarsing.netlify.app"
 			    ));
